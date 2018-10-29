59dcee1d320334c257e132178cc0d5d038bc200c
walz0 <aidan.m.walz@gmail.com>
2018-10-28 23:27:08 -0400

    Added Time Functionality

--- grapher.js
+++ grapher.js
@@ -1,14 +1,24 @@
 var input = document.getElementById("input");
 
-var axes = {}; 
+var axes = {};
+
+var t = 0;
 
 var functions = [
-  function fun1(x) { return Math.sin(x); }, 
-  function fun2(x) { return Math.cos(3 * x); }
+  function fun1(x) { return Math.sin(x / 2); }, 
+  function fun2(x) { return Math.cos(3 * x + t); }
 ];
 
+//60 Frames per second
+var interval = setInterval(timedInterval, 16.7);
+
+function timedInterval () {
+  t += 0.05;
+  draw();
+}
+
 function addFunction() {
-  input = document.getElementById("input");
+  /*input = document.getElementById("input");
   if(input.value != "")  
   {
     var div = document.createElement("div");
@@ -16,7 +26,7 @@ function addFunction() {
     var t = document.createTextNode(input.value);
     div.appendChild(t);
     document.body.appendChild(div);
-  }
+  }*/
 }
 
 function updateScale() {
@@ -29,13 +39,14 @@ function updateScale() {
 
 function draw() {
   var canvas = document.getElementById("canvas");
+  canvas.width = screen.width - 100;
+  canvas.height = screen.height - 250;
   if (null == canvas || !canvas.getContext) return;
 
   var ctx = canvas.getContext("2d");
   axes.scale = document.getElementById("scale").value;
   axes.x0 = .5 + .5 * canvas.width;  // x0 pixels from left to x=0
   axes.y0 = .5 + .5 * canvas.height; // y0 pixels from top to y=0
-  axes.doNegativeX = true;
 
   showAxes(ctx, axes);
   plot(ctx, axes, functions[0], "rgb(139, 233, 253)", 2); 
@@ -45,20 +56,22 @@ function draw() {
 function plot (ctx, axes, func, color, thickness) {
   var xx, //The current 'x' value that is being plotted
   yy, //The current 'y' value that is being plotted
-  dx = 0.01, //The distance 'x' between each point that is plotted
+  dx = 3, //The distance 'x' between each point that is plotted
   x0 = axes.x0, //The 'x' value of the origin
   y0 = axes.y0, //The 'y' value of the origin
   scale = axes.scale; //The scale of the viewport
 
+  //The range of x values of which to loop through
   var iMax = Math.round((ctx.canvas.width - x0) / dx);
-  var iMin = axes.doNegativeX ? Math.round(-x0 / dx) : 0;
+  var iMin = Math.round(-x0 / dx);
 
   ctx.beginPath();
   ctx.lineWidth = thickness;
   ctx.strokeStyle = color;
 
+  //Plotting and evaluating the function at each point x
   for (var i = iMin; i <= iMax; i++) {
-    xx = dx * i; 
+    xx = dx * i;
     yy = scale * func(xx / scale);
 
     if (i == iMin) {
@@ -66,23 +79,27 @@ function plot (ctx, axes, func, color, thickness) {
     }
     else {
       ctx.lineTo(x0 + xx, y0 - yy);
-    }           
+    }
   }
   ctx.stroke();
 }
 
 function showAxes(ctx, axes) {
-  var x0 = axes.x0, w = ctx.canvas.width;
-  var y0 = axes.y0, h = ctx.canvas.height;
-  var xmin = axes.doNegativeX ? 0 : x0;
+  var x0 = axes.x0
+  var w = ctx.canvas.width;
+
+  var y0 = axes.y0
+  var h = ctx.canvas.height;
+
+  var xmin = 0;
   
   ctx.beginPath();
-  ctx.strokeStyle = "rgb(128, 128, 128)"; 
+  ctx.strokeStyle = "rgb(128, 128, 128)";
 
-  ctx.moveTo(xmin, y0); 
+  ctx.moveTo(xmin, y0);
   ctx.lineTo(w, y0);  // X axis
 
-  ctx.moveTo(x0, 0);    
+  ctx.moveTo(x0, 0);
   ctx.lineTo(x0, h);  // Y axis
 
   ctx.stroke();
